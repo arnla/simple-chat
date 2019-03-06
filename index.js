c65d7d5121a81841011bafca1fbbb25d62e39810
Angela <angelaranola30@gmail.com>
2019-03-05 23:44:35 -0700

    Users assigned usernames upon joining the chat.

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+users = [];
+messages = [];
+ctr = 1;
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  socket.on('user join', function() {
+    socket.username = 'user' + ctr;
+    ctr++;
+    users.push(socket.username);
+    console.log(socket.username + ' has joined the chat');
+  });
+
+  socket.on('disconnect', function(data) {
+    users.splice(users.indexOf(socket.username), 1);
+    console.log(socket.username + ' has left the chat');
+  });
+
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
